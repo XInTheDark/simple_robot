c5169c0a8bee8dc9d2b1fa530cbb93cad87cc0ec
J Jerry <61100393+XInTheDark@users.noreply.github.com>
2020-07-19 12:23:04 +0800

    Updated robot.py (Overhaul)
    Update:
    -Fixed serious issue
    ===WORKING ON===
    -Translate section error

--- robot.py
+++ robot.py
@@ -1,4 +1,6 @@
 # Make a simple robot assistant
+# Something wrong with make_response
+# Still not fixed
 
 
 def break_up(self):
@@ -7,8 +9,6 @@ def break_up(self):
 
 
 def make_response(self):
-    global recognised, get_cmd
-    import os
     recognised = False
     get_cmd = False
     database = {
@@ -19,7 +19,7 @@ def make_response(self):
         "quit": "Bye. See you next time!",
         "exit": "Bye. See you next time!",
         "bye": "Bye. See you next time!",
-        "joke": """Here's a joke:
+        "tell joke": """Here's a joke:
 How much wood would a woodchuck chuck if a woodchuck could chuck wood?
 Answer: As many cookies as Cookie Monster could muster if Cookie Monster could master cooking cookies.""",
         "nice": "Thank you!",
@@ -34,7 +34,7 @@ Answer: As many cookies as Cookie Monster could muster if Cookie Monster could m
         "lol": "What's funny?",
         "funny": "What's funny?",
         "haha": "What's funny?",
-        "story": """Okay, here's a story.
+        "tell story": """Okay, here's a story.
 Once upon a time...
 Okay, I don't remember.""",
         "rap": """I know a bit of how to rap:
@@ -46,70 +46,71 @@ Where’s the peck of pickled peppers Peter Piper picked?
 She sells seashells by the seashore,
 How can a clam cram in a clean cream can?
 Can it can a can as a canner can can a can?""",
-        "dirty": """I know how to talk dirty:
+        "talk dirty": """I know how to talk dirty:
 The carpet needs vacuuming.
 Humus. Compost. Pumice. Silt. Gravel.""",
-        "translate": "Opening translator..."
+        "translate": "Opening translator...",
+        "help": "Say something...",
+        "cmd": "Opening cmd..."
     }
     # print(database.get(self))
 
-    words = break_up(self)
+    self = self.lower()
     # print('words',words)
-    for i in words:
-        i = i.lower()
+    for i in database:
         # print(i)
-        if i == 'cmd':
-            get_cmd = True
-        else:
-            get_cmd = False
-
-        if i in database:
+        if i in self:
             recognised = True
             value = database.get(i)
             print(value)
+
+    if self == 'cmd':
+        get_cmd = True
+
+    elif self == "bye" or self == "goodbye" or self == "quit" or self == "exit":
+        exit()
+
+    elif self == 'access_database':
+        print('To access my database, enter the password.')
+        password = input('>')
+        if password == 'jmz118118':
+            print('Database:')
+            print(database)
         else:
-            if not recognised:
-                recognised = False
+            print('Invalid password')
 
-        if i == "bye" or i == "goodbye" or i == "quit" or i == "exit":
-            exit()
-
-        # Translate error
-        if i == "translate":
-            words.remove(i)
-            from PyDictionary import PyDictionary
-            dictionary = PyDictionary()
-            key = input("Enter 'meaning', 'translate', 'antonym', or 'synonym' to get the respective results: ")
-            key = key.lower()
-            if key == 'meaning':
-                print(dictionary.meaning(str(words)))
-            elif key == 'translate':
-                print(dictionary.translate(str(words), 'zh'))
-            elif key == 'antonym':
-                print(dictionary.antonym(str(words)))
-            elif key == 'synonym':
-                print(dictionary.synonym(str(words)))
-            else:
-                print('Invalid!')
-            print("""\nProviders:
-Meanings: WordNet
-Translation: Google Translate
-Synonyms & Antonyms: thesaurus.com""")
+    # Translate error
+    elif self == "translate":
+        print("Translate function is not working. We are fixing this issue.")
+#         self = self.replace("translate", "")
+#         from PyDictionary import PyDictionary
+#         dictionary = PyDictionary()
+#         key = input("Enter 'meaning', 'translate', 'antonym', or 'synonym' to get the respective results: ")
+#         key = key.lower()
+#         if key == 'meaning':
+#             print(dictionary.meaning(str(self)))
+#         if key == 'translate':
+#             print(dictionary.translate(str(self), 'zh'))
+#         elif key == 'antonym':
+#             print(dictionary.antonym(str(self)))
+#         elif key == 'synonym':
+#             print(dictionary.synonym(str(self)))
+#
+#         print("""\nProviders:
+# Meanings: WordNet
+# Translation: Google Translate
+# Synonyms & Antonyms: thesaurus.com""")
 
     if get_cmd:
         import os
         import sys
-        import time
         from termcolor import colored
 
-        print('Opening cmd...')
-        time.sleep(2)
         input_symbol = colored('_', attrs=['reverse', 'blink'])
         file_path = sys.executable
         repeat = True
 
         while repeat:
-            time.sleep(1)
             cmd = input(file_path + input_symbol)
             os.system(cmd)
             if cmd == 'exit':
@@ -119,15 +120,18 @@ Synonyms & Antonyms: thesaurus.com""")
     if not recognised:
         print("Sorry, I don't understand.")
 
-    return get_cmd
-
 
 def robot():
     print('Welcome to robot.py')
     print('What can I do for you?')
+
     while True:
         query = input('>')
         make_response(query)
 
-    # For Test:
-    # print(make_response('hello'))
+
+robot()
+
+# For Test:
+# make_response('hello')
+# make_response('cmd')
